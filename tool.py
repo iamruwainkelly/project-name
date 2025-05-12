2d83ebe52e5ca4cd4fc0e5eb23fbb4cd79fa0b17
iamruwainkelly <info@ruwainkelly.co.za>
2025-05-12 14:46:05 +0200

    Add initial scaffold for tool.py

---
+++ tool.py
@@ -0,0 +1,12 @@
+def welcome():
+    print("welcome to Project CLI!")
+    print("This is your starter script. Time to build")
+
+    def main():
+        welcome()
+        # Add future here
+
+    if __name__ == "__main__":
+        main()
+        # end main
+    
